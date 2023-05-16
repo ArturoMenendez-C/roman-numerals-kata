1ee5ee32a575048694dea29bf9bfb5b7b51ef72a
Arturo Menendez <arturo.menendez@codurance.com>
2023-05-16 12:26:52 +0200

    refactored case for V case

--- src/main/java/RomanNumeralGenerator.java
+++ src/main/java/RomanNumeralGenerator.java
@@ -1,25 +1,28 @@
 public class RomanNumeralGenerator {
 
     public static final String I = "I";
+    public static final String V = "V";
 
     public String convert(int arabic) {
 
         String output = "";
 
-        if (arabic == 5) {
-
-            output = "V";
+        if (arabic >= 5) {
+            output = V;
+            arabic -= 5;
         }
-        if(arabic == 6) {
-            output = "VI";
-        }
-        if(arabic == 7){
-            output = "VII";
-        }
+//        if(arabic == 6) {
+//            output = "V";
+//            output += I;
+//        }
+//        if(arabic == 7){
+//            output = "V";
+//            output += I;
+//            output += I;
+//        }
 
         if (arabic <= 3) {
-
-            output = I.repeat(arabic);
+            output += I.repeat(arabic);
         }
 
         return output;
