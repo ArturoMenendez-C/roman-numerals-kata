eb7569a2f3ca9a0eaa575340a127d9556af35518
Arturo Menendez <arturo.menendez@codurance.com>
2023-05-16 16:41:37 +0200

    refactored code following algorithm suggestions

--- src/main/java/RomanNumeralGenerator.java
+++ src/main/java/RomanNumeralGenerator.java
@@ -1,9 +1,9 @@
 public class RomanNumeralGenerator {
 
     enum ArabicToRoman {
-        ONE(1, "I"),
+        TEN(10, "X"),
         FIVE(5, "V"),
-        TEN(10, "X");
+        ONE(1, "I");
 
         private final int arabic;
         private final String roman;
@@ -18,36 +18,13 @@ public class RomanNumeralGenerator {
     public String convert(int arabic) {
 
         String output = "";
-//
-//        if(arabic >= ArabicToRoman.TEN.arabic*3) {
-//            output += ArabicToRoman.TEN.roman;
-//            arabic -= ArabicToRoman.TEN.arabic;
-//
-//        }
-//        if(arabic >= ArabicToRoman.TEN.arabic*2) {
-//            output += ArabicToRoman.TEN.roman;
-//            arabic -= ArabicToRoman.TEN.arabic;
-//
-//        }
-//
-//        if (arabic >= ArabicToRoman.TEN.arabic) {
-//            output += ArabicToRoman.TEN.roman;
-//            arabic -= ArabicToRoman.TEN.arabic;
-//        }
 
-        while(arabic >= ArabicToRoman.TEN.arabic) {
-            output += ArabicToRoman.TEN.roman;
-            arabic -= ArabicToRoman.TEN.arabic;
-        }
+        for (ArabicToRoman arabicToRomanKey : ArabicToRoman.values()) {
+            while (arabic >= arabicToRomanKey.arabic) {
+                output += arabicToRomanKey.roman;
+                arabic -= arabicToRomanKey.arabic;
+            }
 
-
-        if (arabic >= ArabicToRoman.FIVE.arabic) {
-            output += ArabicToRoman.FIVE.roman;
-            arabic -= ArabicToRoman.FIVE.arabic;
-        }
-
-        if (arabic <= 3) {
-            output += ArabicToRoman.ONE.roman.repeat(arabic);
         }
 
         return output;
