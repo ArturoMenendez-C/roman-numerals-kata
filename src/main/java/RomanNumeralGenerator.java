245a4608b29977213cf448875a794f19d053543d
Rebeca Gonzalez <bequipedia@gmail.com>
2023-05-16 10:59:29 +0200

    refactor 1 of 3 test cases

--- src/main/java/RomanNumeralGenerator.java
+++ src/main/java/RomanNumeralGenerator.java
@@ -1,11 +1,14 @@
 public class RomanNumeralGenerator {
     public String convert(int arabic) {
+
+        String result = "I";
         if(arabic == 2){
-            return "II";
+           result = result.repeat(2);
         }
         if(arabic == 3) {
-            return "III";
+            result = result.repeat(3);
         }
-        return "I";
+        return result;
     }
+
 }
